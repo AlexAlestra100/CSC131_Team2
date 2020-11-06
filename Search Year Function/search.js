78e04760178b7a73333abc422cc9ae1a469868db
esilva2 <72953766+esilva2@users.noreply.github.com>
2020-11-06 01:19:44 -0800

    Update search.js

--- Search Year Function/search.js
+++ Search Year Function/search.js
@@ -22,13 +22,15 @@ async function searchData(){
     {
        numOfMovies++;       // increments each time search for year is found
          
+        document.write(' Category:   ' + data[i].category + ' Entity:   ' + data[i].entity + ' Year:   ' + data[i].year);
+       document.write("<br><br>");
         console.log(data[i].year);
         
     }
     
 }
 
-document.write(" Number of Movies nominated in " + inYear + " is " + numOfMovies);
+document.write(" Number of Movies nominated in the year" + inYear + " is " + numOfMovies);
 
 }
 
