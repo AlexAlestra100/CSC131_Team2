4e78fda3332258ce1ab211bb85c0b19614c4ae7f
AlexAlestra100 <AlexAlestra100@users.noreply.github.com>
2020-11-06 16:42:33 -0800

    corrected

--- Search Category Function/CategorySearch.js
+++ Search Category Function/CategorySearch.js
@@ -1,7 +1,7 @@
 const database = 'https://github.com/AlexAlestra100/CSC131_Team2/blob/main/refrence.json';
 
 async function categorySearch(){
-    const resposne = await fetch(database);
+    const response = await fetch(database);
     const data = await response.json();
 
     for(let i = 0; i < data.length; i++){
