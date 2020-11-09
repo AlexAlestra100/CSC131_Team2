66f45cdacbb2b809242c871c01b7b8d458d2662b
DannySteele67 <72956146+DannySteele67@users.noreply.github.com>
2020-11-09 13:21:38 -0800

    Update CategorySearch.js

--- Search Category Function/CategorySearch.js
+++ Search Category Function/CategorySearch.js
@@ -1,13 +1,33 @@
-const database = 'testDataJson.json';
 
 async function categorySearch(searchKey){
+    const database = 'testDataJson.json';
     const response = await fetch(database);
     const data = await response.json();
+    let searchResult =[];
+    let resultCounter = 0;
 
     for(let i = 0; i < data.length; i++){
-        if(data[i].category === searchKey)
-            console.log(data[i].category + ' ' + data[i].entity + ' ' + data[i].winner + ' ' + data[i].year);
+        if(data[i].category === searchKey){
+
+            //set current point in array as an object
+            searchResult[resultCounter] = {};
+
+            //adds in the results from the database to object in the array
+            searchResult[resultCounter].category = data[i].category;
+            searchResult[resultCounter].entity = data[i].entity;
+            searchResult[resultCounter].winner = data[i].winner;
+            searchResult[resultCounter].year = data[i].year;
+            
+            //loging search results to check
+            //console.log(searchResult[resultCounter].category + ' ' + searchResult[resultCounter].entity + ' ' + searchResult[resultCounter].winner + ' ' + searchResult[resultCounter].year);
+
+            resultCounter++;
+            //logging database for checking
+            //console.log(data[i].category + ' ' + data[i].entity + ' ' + data[i].winner + ' ' + data[i].year);
+        
+        }
     }
+    return searchResult;
 }
 
-categorySearch("ENGINEERING EFFECTS");
+categorySearch("ACTOR");
