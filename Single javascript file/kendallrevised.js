67af11c3a72cd59cc9f708ee9c349874ae5dcd08
kchelms95 <37390827+kchelms95@users.noreply.github.com>
2020-11-18 22:29:04 -0800

    Update kendallrevised.js

--- Single javascript file/kendallrevised.js
+++ Single javascript file/kendallrevised.js
@@ -1,130 +1,63 @@
-//Importing the json up here
-//THIS GLOBAL FUNCTION DOSENT WORK!!!!!
 async function importingJson(){
     const database = './data.json';
     const response = await fetch(database);
     const data = await response.json();
-    return data;//returning json obj
+    return data;
  } 
  
- 
- //win function,accepts bool true or false
-function winnerSearch(oscarsData, searchKey){
+ async function combinedSearch(inputJSON) {
+    let result = await importingJson();
 
-const data = importingJson();// data set to json //DOSENT WORK
+    if(inputJSON.winlose) result = winnerSearch(result, inputJSON.winlose);
+    if(inputJSON.year) result = yearSearch(result, inputJSON.year);
+    if(inputJSON.category) result = categorySearch(result, inputJSON.category);
+    if(inputJSON.entity) result = entitySearch(result, inputJSON.entity);
+
+    return result;
+ }
+ 
+function winnerSearch(oscarsData, searchKey){
     let searchResult =[];
 
-    //used for testing function logic
-    let resultCounter = 0;
-
     for(let i = 0; i < oscarsData.length; i++){
-        if(oscarsData[i].winner == searchKey){ //searchkey = true in html file
-
-            searchResult.push(data[i]);         
+        if(oscarsData[i].winner == searchKey){ 
+            searchResult.push(oscarsData[i]);         
         }
     }
     return searchResult;
 }
 
- //Kendall: make sure you're testing code outside the function
- importingJson().then(data => console.log(winnerSearch(data, true)));
+function yearSearch(oscarsData,  searchKey){
+    let searchResult =[];
 
+    for(let i = 0; i < oscarsData.length; i++){
+        if(oscarsData[i].year == searchKey){ 
+            searchResult.push(oscarsData[i]);         
+        }
+    }
+    return searchResult;
+}
 
- /*
- //categorysearch
- async function categorySearch(searchKey){
-     const database = 'testDataJson.json';
-     const response = await fetch(database);
-     const data = await response.json();
-     let searchResult =[];
-     //used for testing function logic
-     //let resultCounter = 0;
-     for(let i = 0; i < data.length; i++){
-         if(data[i].category === searchKey){
-             //adds in the results from the database to object in the array
-             searchResult.push(data[i]);
-             
-             
-         
-         }
-     }
-     return searchResult;
- }
- //reworked name functoin
- async function nameSearch(searchKey){
-    // const database = 'testDataJson.json';
-    // const response = await fetch(database);
-    // const data = await response.json();
-     let searchResult =[];
-     //used for testing function logic
-     //let resultCounter = 0;
-     for(let i = 0; i < data.length; i++){
-         if(data[i].name === searchKey){
-             //adds in the results from the database to object in the array
-             searchResult.push(data[i]);
-             
-            
-         
-         }
-     }
-     return searchResult;
- }
- //name, also lets you search
- let DATA = [];
- window.addEventListener('load', () => {
-     fetch("data.json")
-         .then(res => {
-             return res.json()
-         })
-         .then(Res => {
-             DATA = (Res)
-             SearchByName(title)
-         })
-         .catch(err => {
-             console.log('cant fetch the JSON');
-         })
- })
- let title = window.prompt("Enter Title Name");
- let SearchByName = async(name) => {
-     let FilteredData = DATA.filter(a => a.entity == name ? a : null)
-     FilteredData.forEach(data => {
-         console.log('Entity: ' + data.entity + " Year: " + data.year + " Category: " + data.category, " Winner " + data.winner);
-     })
- }
- const obj = JSON.parse(JSON.stringify('refrence.json'));
- // copy data from object to another object
- const obj2 = [{}]; // empty object that will store search results of object
- //Year function, needs fixed input
- var inYear = window.prompt('Input year');
- var numOfMovies = 0;
- async function searchYear(){
-     const response = await fetch(obj);
-     const data = await response.json();
-     const {year} = data;//maybe dont need this
-     //document.getElementById("A").textContent = year;
-     
-     document.write('Year : ' + inYear);
-     for (let i= 0; i < data.length; i++)
-      {
-     //console.log(data[i].year);
-     
-     if(inYear == data[i].year)
-     {
-         Object.assign(obj2, data[i])
-       
-        numOfMovies++;       // increments each time search for year is found
-        // document.write(' Category:   ' + data[i].category + ' Entity:   ' + data[i].entity + ' Year:   ' + data[i].year);
-        //document.write("<br><br>");
-        // console.log(data[i].year);
-         console.log(obj2);
-         document.write("<br><br>");
-         document.write ('category: ' + obj2.category + ' entity: ' + obj2.entity + ' winner: ' + obj2.winner + ' year: ' + obj2.year);
-        
-     }
-     
- }
- console.log("/n");
- document.write(" Number of Movies nominated in the year " + inYear + " is " + numOfMovies);
- }
- searchYear();
- */
+function categorySearch(oscarsData,  searchKey){
+    let searchResult =[];
+
+    for(let i = 0; i < oscarsData.length; i++){
+        if(oscarsData[i].category == searchKey){ 
+
+            searchResult.push(oscarsData[i]);         
+        }
+    }
+    return searchResult;
+}
+
+function entitySearch(oscarsData,  searchKey){
+    let searchResult =[];
+
+    for(let i = 0; i < oscarsData.length; i++){
+        if(oscarsData[i].entity == searchKey){ 
+
+            searchResult.push(oscarsData[i]);         
+        }
+    }
+    return searchResult;
+}
