4f93c7c1e2ddaf62010b51642ae0c8a822dd8a97
DannySteele67 <72956146+DannySteele67@users.noreply.github.com>
2020-11-21 16:08:52 -0800

    Create omdbCall.js

---
+++ omdbCall/omdbCall.js
@@ -0,0 +1,29 @@
+async function apiRequest(searchResults){
+    const apiKey = "903ccd22";
+
+    for(let i = 0; i < searchResults.length; i++){
+
+    let url ="http://www.omdbapi.com/?apikey=" + apiKey + "&t=" + searchResults[i].entity;
+ 
+
+    let response = await fetch(url);
+    let data = await response.json();
+
+    if(data.Response === "False"){
+        console.log('No movie title given');
+    }
+    else{
+        searchResults[i].imdbLink = 'https://www.imdb.com/title/' + data.imdbID;
+        searchResults[i].director = data.Director;
+        searchResults[i].rating = data.Ratings;
+        searchResults[i].plot = data.Plot;
+        searchResults[i].time = data.Runtime;
+
+    }
+
+
+    }
+
+    return searchResults;
+
+}
