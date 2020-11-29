026853bf877ef8bb316c5b6ba8ccd8679c90f18f
kchelms95 <37390827+kchelms95@users.noreply.github.com>
2020-11-29 10:30:49 -0800

    Further Organized / Cleaned Up

--- rest.api/routes.js
+++ rest.api/routes.js
@@ -1,6 +1,6 @@
 //Anytime there is a GET request to the root app
 //It will print out the test code
-let search = require("../database/search.js")
+let search = require("./database/oscarsData_search.js")
 
 let testObj = {'winner' : 1, 'year' : [1999, 1992]};
 
