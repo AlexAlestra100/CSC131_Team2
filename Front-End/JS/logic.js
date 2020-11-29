6ea286aa3c7f11b95e5cbcee62e8d0b7d830c2fe
AlexAlestra100 <AlexAlestra100@users.noreply.github.com>
2020-11-29 11:26:43 -0800

    update to coed

--- Front-End/JS/logic.js
+++ Front-End/JS/logic.js
@@ -1,23 +1,18 @@
-class User
-{
-    this.name = name;
-    this.years = years;
-    this.cat = cat.
-    this.win = win;
-}
-
-var name = "Dave";
-var years = [12, 13, 14];
-var cat = ["Saab", "Volvo", "BMW"];
-var win = false;
-
 function findYear()
 {
-    var yearStart = 1927;
-    var yearEnd = 1934;
+    var yearStart = document.getElementById('m_start');
+    var yearEnd = document.getElementById('m_end');
     
     for(var i = yearStart; i <= yearEnd; i++)
     {
         years.push(i);
     }
+}
+
+fucntion result()
+{
+    var name = document.getElementById('m_name');
+    var categories = document.getElementById('cars');
+    var year = findYear();
+    var wol = document.getElementById('wol');
 }
