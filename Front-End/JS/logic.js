c6829dcd846f1f8c2afe14d0c1cb2918f8c78af2
AlexAlestra100 <AlexAlestra100@users.noreply.github.com>
2020-11-29 10:43:34 -0800

    intro to logic

--- Front-End/JS/logic.js
+++ Front-End/JS/logic.js
@@ -1,8 +1,23 @@
-var slider = document.getElementById("myRange");
-var output = document.getElementById("data");
-output.innerHTML = slider.value;
-
-slider.oninput = function()
+class User
 {
-  output.innerHTML = this.value;
+    this.name = name;
+    this.years = years;
+    this.cat = cat.
+    this.win = win;
+}
+
+var name = "Dave";
+var years = [12, 13, 14];
+var cat = ["Saab", "Volvo", "BMW"];
+var win = false;
+
+function findYear()
+{
+    var yearStart = 1927;
+    var yearEnd = 1934;
+    
+    for(var i = yearStart; i <= yearEnd; i++)
+    {
+        years.push(i);
+    }
 }
