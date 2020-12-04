d67d46be509977e8590272bc114305c7b3b9c64b
DannySteele67 <72956146+DannySteele67@users.noreply.github.com>
2020-12-03 19:04:06 -0800

    Update externalApiCall.js

--- API/Database/externalApiCall.js
+++ API/Database/externalApiCall.js
@@ -1,4 +1,4 @@
-function singleton(singleSearch){
+async function singleton(singleSearch){
     const omdbKey = '903ccd22';
     const tmdbKey = '2c1f532b5ae9cd29e9cd937bcc1861b7';
     let fetch = require('node-fetch');
@@ -6,52 +6,50 @@ function singleton(singleSearch){
     let omdbUrl = "http://www.omdbapi.com/?apikey=" + omdbKey + "&t=" + singleSearch.movie;
     let tmdbUrl = 'https://api.themoviedb.org/3/movie/' + singleSearch.tid + '?api_key=' + tmdbKey;
 
-    //Fetching Ratings, plot synopsis, and other info
-    fetch(omdbUrl)
-    .then(res => res.json())
-    .then(json =>
-        {
-            //check if OMDB call doesn't have error/if call limit is reached
-            if(json.hasOwnProperty('Error')){
-                fetch(tmdbUrl)
-                .then(res => res.json())
-                .then(json => {
+    //fetch ratings, plot, runtime, and other
+    let res = await fetch(omdbUrl);
+    let json = await res.json();
+    if(json.hasOwnProperty('Error')){
+        fetch(tmdbUrl)
+        .then(res => res.json())
+        .then(json => {
 
-                    singleSearch.popularity = json.popularity;
-                    singleSearch.plot = json.overview;
-                    singleSearch.time = json.runtime;
-                })
-            }
-            else{
-            singleSearch.ratings = json.Ratings;
-            singleSearch.plot = json.Plot;
-            singleSearch.time = json.Runtime;
-            singleSearch.director = json.Director;
-            singleSearch.cast = json.Actors;
-            
+            singleSearch.popularity = json.popularity;
+            singleSearch.plot = json.overview;
+            singleSearch.time = json.runtime;
+        })
+    }
+    else{
+    singleSearch.ratings = json.Ratings;
+    singleSearch.plot = json.Plot;
+    singleSearch.time = json.Runtime;
+    singleSearch.director = json.Director;
+    singleSearch.cast = json.Actors;
+    }
 
-
-            }
-        });
-    //fetch watch providers
     tmdbUrl = 'https://api.themoviedb.org/3/movie/' + singleSearch.tid + '/watch/providers?api_key=' + tmdbKey;
-    fetch(tmdbUrl)
-    .then(res => res.json())
-    .then(json => {
-        if(!json.results.hasOwnProperty('US')){
-            singleSearch.watchProvider = "No Watch Providers Available";
+    
+    //fetch wath providers
+    res = await fetch(tmdbUrl) 
+    json = await res.json();
+    
+    if(!json.results.hasOwnProperty('US')){
+        singleSearch.watchProvider = "No Watch Providers Available";
+    }
+    else{
+        singleSearch.watchProvider = json.results.US.link;
         }
-        else{
-            singleSearch.watchProvider = json.results.US.link;
-            }
-            
-    })
-    
-    
-}
 
+    return singleSearch;
+
+    }
+
+async function singletonDeliver(someObject){
+    let data = await singleton(someObject);
+
+    return data;
+}
 
 let testObj = {"movie": "The Patent Leather Kid", "tid": 68718}
 
-let test = singleton(testObj);
-console.log(test);
+singletonDeliver(testObj).then(data => console.log(data));
