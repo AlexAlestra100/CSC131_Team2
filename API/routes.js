3af40514b266f942fde47420d79626932f245716
kchelms95 <37390827+kchelms95@users.noreply.github.com>
2020-12-03 19:26:35 -0800

    basic routes idea

--- API/routes.js
+++ API/routes.js
@@ -1,17 +1,36 @@
 //Anytime there is a GET request to the root app
 //It will print out the test code
-let search = require("./database/oscarsData_search.js")
-
-let testObj = {'winner' : 1, 'year' : [1999, 1992]};
+//let validate    = require("./");
+let search      = require("./database/oscarsData_search.js")
 
 var appRouter = function(app){
     app.get("/",function(req,res){
-        res.status(200).send("This is the restful check");
+        if(req.query.ID){ 
+            let ID = req.query.ID;
+
+            ID = {'ID' : parseInt(ID)};
+
+            res.status(200).send(search(ID));
+        }
+
+        else
+            res.status(200).send(req.query);
     });
     
-    app.get("/test",function(req,res){
-        res.status(200).send(search(testObj));
-    });    
+    app.get('/search/',function(req,res){
+      
+        let queryOBJ = {};
+
+        queryOBJ.winner             = 1 + parseInt(req.query.w);
+        queryOBJ.year               = req.query.yr;     
+        queryOBJ.general_cat        = req.query.gc;
+        queryOBJ.category           = req.query.c;
+        queryOBJ.entity             = req.query.e;
+
+        //queryOBJ = validate(queryOBJ);
+        
+        res.status(200).send(search(queryOBJ));
+    });
 }
 
 module.exports = appRouter;
